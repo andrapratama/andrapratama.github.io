9fed94ba09409ab158b32ef97ecf5aa6dc514bfd
andrasatriapratama <andrasatriapratama@gmail.com>
2023-04-25 19:53:09 +0700

    remake themes

--- js/myscripts.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("tutorial");
-
-if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    // drawing code here
-} else {
-    // canvas-unsupported code here
-}
